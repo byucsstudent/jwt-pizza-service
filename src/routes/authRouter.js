adfd318bd72b7ec7cae16fe41f3d7589b069a99e
Lee <leesjensen@gmail.com>
2024-11-06 16:26:18 -0700

    fix(metrics) user and auth counts

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config.js');
 const { asyncHandler } = require('../endpointHelper.js');
 const { DB, Role } = require('../database/database.js');
+const metrics = require('../metrics');
 
 const authRouter = express.Router();
 
@@ -67,17 +68,17 @@ authRouter.authenticateToken = (req, res, next) => {
 authRouter.post(
   '/',
   asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
     try {
-      const { name, email, password } = req.body;
       if (!name || !email || !password) {
         return res.status(400).json({ message: 'name, email, and password are required' });
       }
       const user = await DB.addUser({ name, email, password, roles: [{ role: Role.Diner }] });
       const auth = await setAuth(user);
       res.json({ user: user, token: auth });
-      loginEvent(user.id, true);
+      metrics.loginEvent(email, true);
     } catch (error) {
-      loginEvent(user.id, false);
+      metrics.loginEvent(email, false);
       throw error;
     }
   })
@@ -87,14 +88,14 @@ authRouter.post(
 authRouter.put(
   '/',
   asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
     try {
-      const { email, password } = req.body;
       const user = await DB.getUser(email, password);
       const auth = await setAuth(user);
       res.json({ user: user, token: auth });
-      loginEvent(user.id, true);
+      metrics.loginEvent(email, true);
     } catch (error) {
-      loginEvent(user.id, false);
+      metrics.loginEvent(email, false);
       throw error;
     }
   })
